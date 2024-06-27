211273b0c66eb7c42f710e650e9f6ae970c2351a
aditya kumar <adityakr2019@gmail.com>
2024-06-27 23:04:22 +0530

    comit add html

--- project/project/settings.py
+++ project/project/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
